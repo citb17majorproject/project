e0d421e51b64c772940c7e9793e07cf09ab428b7
citb17majorproject <87939078+citb17majorproject@users.noreply.github.com>
2021-07-26 16:21:02 +0530

    Update policy.go

--- chaincode/go/m/policy.go
+++ chaincode/go/m/policy.go
@@ -26,7 +26,7 @@ type AO struct {
 }
 
 type AE struct {
-	CreatedTime int64  `json:"createdTime"`
+	StartTime int64  `json:"StartTime"`
 	EndTime     int64  `json:"endTime"`
 	AllowedIP   string `json:"allowedIP"`
 }
